efdc3918acf17c3f1162a4bc4a0f4c5dfee3e57e
Javier Choque <javier.choque@uatf.edu.bo>
2026-08-21 13:11:54 -0400

    #7 - Setup Backend

---
+++ backend/vsiaf/src/main/java/com/uatf/vsiaf/VsiafApplication.java
@@ -0,0 +1,13 @@
+package com.uatf.vsiaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsiafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsiafApplication.class, args);
+	}
+
+}
---
+++ backend/vsiaf/src/test/java/com/uatf/vsiaf/VsiafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uatf.vsiaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsiafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
